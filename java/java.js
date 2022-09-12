642ab607d9127cf501fc78dac1f1fa2edc55727b
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-12 12:40:30 -0700

    parallel refactoring

--- java/java.js
+++ java/java.js
@@ -1,13 +1,28 @@
 
-async function get3Pokemon(){
-    const poke1 = await axios.get ('https://pokeapi.co/api/v2/pokemon/1');
-    const poke2 = await axios.get ('https://pokeapi.co/api/v2/pokemon/2');
-    const poke3 = await axios.get ('https://pokeapi.co/api/v2/pokemon/3');
+// async function get3Pokemon(){                         //sequential requests
+//     const poke1 = await axios.get ('https://pokeapi.co/api/v2/pokemon/1');
+//     const poke2 = await axios.get ('https://pokeapi.co/api/v2/pokemon/2');
+//     const poke3 = await axios.get ('https://pokeapi.co/api/v2/pokemon/3');
+//     console.log(poke1.data);
+//     console.log(poke2.data);
+//     console.log(poke3.data);
+// }
+// get3Pokemon();
+
+
+async function get3Pokemon(){                   //parallel calls
+    const prom1 =  axios.get ('https://pokeapi.co/api/v2/pokemon/1');
+    const prom2 =  axios.get ('https://pokeapi.co/api/v2/pokemon/2');
+    const prom3 =  axios.get ('https://pokeapi.co/api/v2/pokemon/3');
+    console.log(prom1);
+    const poke1 = await prom1;
+    const poke2 = await prom2;
+    const poke3 = await prom3;
+    console.log(prom1);
     console.log(poke1.data);
     console.log(poke2.data);
     console.log(poke3.data);
 }
 get3Pokemon();
-
 
 
