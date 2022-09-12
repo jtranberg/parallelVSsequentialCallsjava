91cc187ffba0f12162117aa47026e407f7e5999b
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-12 12:56:11 -0700

    sequential light show

--- java/java.js
+++ java/java.js
@@ -10,19 +10,36 @@
 // get3Pokemon();
 
 
-async function get3Pokemon(){                   //parallel calls
-    const prom1 =  axios.get ('https://pokeapi.co/api/v2/pokemon/1');
-    const prom2 =  axios.get ('https://pokeapi.co/api/v2/pokemon/2');
-    const prom3 =  axios.get ('https://pokeapi.co/api/v2/pokemon/3');
-    console.log(prom1);
-    const poke1 = await prom1;
-    const poke2 = await prom2;
-    const poke3 = await prom3;
-    console.log(prom1);
-    console.log(poke1.data);
-    console.log(poke2.data);
-    console.log(poke3.data);
+// async function get3Pokemon(){                   //parallel calls
+//     const prom1 =  axios.get ('https://pokeapi.co/api/v2/pokemon/1');
+//     const prom2 =  axios.get ('https://pokeapi.co/api/v2/pokemon/2');
+//     const prom3 =  axios.get ('https://pokeapi.co/api/v2/pokemon/3');
+//     console.log(prom1);
+//     const poke1 = await prom1;
+//     const poke2 = await prom2;
+//     const poke3 = await prom3;
+//     console.log(prom1);
+//     console.log(poke1.data);
+//     console.log(poke2.data);
+//     console.log(poke3.data);
+// }
+// get3Pokemon();
+
+function changeBodyColor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout (() => {
+        document.body.style.backgroundColor = color;
+        resolve();
+    }, delay);
+});
 }
-get3Pokemon();
+async function lightShow(){
+    await changeBodyColor('teal', 1000);
+    await changeBodyColor('red', 1000);
+    await changeBodyColor('blue', 1000);
+    await changeBodyColor('green', 1000);
+    await changeBodyColor('violet', 1000);
+}
+lightShow();
 
 
