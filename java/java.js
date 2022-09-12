c3b0f874e99c8b021f4a43b251078adf38be531b
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-12 13:19:53 -0700

    promise.all with for loop to refactor parallel requests with await

--- java/java.js
+++ java/java.js
@@ -61,7 +61,13 @@ async function get3Pokemon(){                         //sequential requests
     const prom2 =  axios.get ('https://pokeapi.co/api/v2/pokemon/2');
     const prom3 =  axios.get ('https://pokeapi.co/api/v2/pokemon/3');
     const results = await Promise.all([prom1, prom2, prom3,]);
-    console.log(results);
+    printPokemon(results);
+    
+}
+function printPokemon(results){
+   for(let pokemon of results){
+    console.log(pokemon.data.name);
+   }
 }
 get3Pokemon();
 
