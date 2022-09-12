f5cc59274d451dc4d80f12b14dababc1f6e29057
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-12 13:12:27 -0700

    await promise.all to get an array result

--- java/java.js
+++ java/java.js
@@ -25,14 +25,14 @@
 // }
 // get3Pokemon();
 
-function changeBodyColor(color, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout (() => {
-        document.body.style.backgroundColor = color;
-        resolve();
-    }, delay);
-});
-}
+// function changeBodyColor(color, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout (() => {
+//         document.body.style.backgroundColor = color;
+//         resolve();
+//     }, delay);
+// });
+// }
 // async function lightShow(){
 //     await changeBodyColor('teal', 1000);
 //     await changeBodyColor('red', 1000);
@@ -40,20 +40,29 @@ function changeBodyColor(color, delay) {
 //     await changeBodyColor('green', 1000);
 //     await changeBodyColor('violet', 1000);
 // }
-async function lightShow() {                     //so fast all you see is last color, no delay
-    const p1 = changeBodyColor('teal', 1000);
-    const p2 =  changeBodyColor('red', 1000);
-    const p3 =  changeBodyColor('blue', 1000);
-    const p4 =  changeBodyColor('blue', 1000);
-    const p5 =  changeBodyColor('green', 1000);
-    const p6 =  changeBodyColor('violet', 1000);
-    await p1;
-    await p2;
-    await p3;
-    await p4;
-    await p5;
-    await p6;
+// async function lightShow() {                     //so fast all you see is last color, no delay
+//     const p1 = changeBodyColor('teal', 1000);
+//     const p2 =  changeBodyColor('red', 1000);
+//     const p3 =  changeBodyColor('blue', 1000);
+//     const p4 =  changeBodyColor('blue', 1000);
+//     const p5 =  changeBodyColor('green', 1000);
+//     const p6 =  changeBodyColor('violet', 1000);
+//     await p1;
+//     await p2;
+//     await p3;
+//     await p4;
+//     await p5;
+//     await p6;
+// }
+// lightShow();
+
+async function get3Pokemon(){                         //sequential requests
+    const prom1 =  axios.get ('https://pokeapi.co/api/v2/pokemon/1');
+    const prom2 =  axios.get ('https://pokeapi.co/api/v2/pokemon/2');
+    const prom3 =  axios.get ('https://pokeapi.co/api/v2/pokemon/3');
+    const results = await Promise.all([prom1, prom2, prom3,]);
+    console.log(results);
 }
-lightShow();
+get3Pokemon();
 
 
