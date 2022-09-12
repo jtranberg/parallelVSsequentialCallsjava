6e143dba2f1fce405459be57edbf793229aa826d
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-12 12:29:40 -0700

    first commint for squention async calls

---
+++ java/java.js
@@ -0,0 +1,13 @@
+
+async function get3Pokemon(){
+    const poke1 = await axios.get ('https://pokeapi.co/api/v2/pokemon/1');
+    const poke2 = await axios.get ('https://pokeapi.co/api/v2/pokemon/2');
+    const poke3 = await axios.get ('https://pokeapi.co/api/v2/pokemon/3');
+    console.log(poke1.data);
+    console.log(poke2.data);
+    console.log(poke3.data);
+}
+get3Pokemon();
+
+
+
